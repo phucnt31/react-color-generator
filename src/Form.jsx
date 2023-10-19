4f99a340fb3b266a65b32cdfc0af4de381d93e2d
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-19 18:48:06 +0700

    Setup form component

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  const [color, setColor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="container">
+      <form className="color-form" onSubmit={handleSubmit}>
+        <h4>color generator</h4>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="#f15025"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          submit
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
