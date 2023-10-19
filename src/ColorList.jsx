99196c447f556cd2561345934ce6f3627c5bdfaa
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-19 18:57:15 +0700

    Color rendered

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import SingleColor from "./SingleColor";
+import { nanoid } from "nanoid";
 
 const ColorList = ({ colors }) => {
-  return <div>ColorList</div>;
+  return (
+    <article className="colors">
+      {colors.map((color) => {
+        return <SingleColor key={nanoid()} color={color} />;
+      })}
+    </article>
+  );
 };
 
 export default ColorList;
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const SingleColor = () => {
-  return <div>SingleColor</div>;
+const SingleColor = ({ color }) => {
+  const { weight, hex } = color;
+
+  return (
+    <div className="color" style={{ background: `#${hex}` }}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}</p>
+    </div>
+  );
 };
 
 export default SingleColor;
