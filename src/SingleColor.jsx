660e794724a0d0eee8db2f1b42e2cdfed5eef142
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-19 19:13:28 +0700

    Add copy to clipboard function

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const SingleColor = ({ color }) => {
   const { weight, hex } = color;
 
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color copied to clipboard");
+      } catch (error) {
+        console.log(error);
+        toast.error(error);
+      }
+    }
+  };
+
   return (
-    <div className="color" style={{ background: `#${hex}` }}>
+    <div
+      className="color"
+      style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
     </div>
